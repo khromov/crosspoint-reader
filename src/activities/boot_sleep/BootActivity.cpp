2466408861e8b0c120b79c5bde762fc3e9f8ca59
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-01-04 18:33:09 +0100

    allow custom logo

--- src/activities/boot_sleep/BootActivity.cpp
+++ src/activities/boot_sleep/BootActivity.cpp
@@ -1,6 +1,8 @@
 #include "BootActivity.h"
 
+#include <Bitmap.h>
 #include <GfxRenderer.h>
+#include <SDCardManager.h>
 
 #include "fontIds.h"
 #include "images/CrossLarge.h"
@@ -12,7 +14,25 @@ void BootActivity::onEnter() {
   const auto pageHeight = renderer.getScreenHeight();
 
   renderer.clearScreen();
-  renderer.drawImage(CrossLarge, (pageWidth + 128) / 2, (pageHeight - 128) / 2, 128, 128);
+
+  // Try to load custom logo from SD card
+  bool customLogoDrawn = false;
+  FsFile file;
+  if (SdMan.openFileForRead("BOOT", "/logo.bmp", file)) {
+    Bitmap bitmap(file);
+    if (bitmap.parseHeaders() == BmpReaderError::Ok) {
+      const int logoX = (pageWidth - bitmap.getWidth()) / 2;
+      const int logoY = (pageHeight - bitmap.getHeight()) / 2;
+      renderer.drawBitmap(bitmap, logoX, logoY, 128, 128);
+      customLogoDrawn = true;
+    }
+    file.close();
+  }
+
+  // Fall back to embedded CrossLarge if custom logo not loaded
+  if (!customLogoDrawn) {
+    renderer.drawImage(CrossLarge, (pageWidth + 128) / 2, (pageHeight - 128) / 2, 128, 128);
+  }
   renderer.drawCenteredText(UI_10_FONT_ID, pageHeight / 2 + 70, "CrossPoint", true, EpdFontFamily::BOLD);
   renderer.drawCenteredText(SMALL_FONT_ID, pageHeight / 2 + 95, "BOOTING");
   renderer.drawCenteredText(SMALL_FONT_ID, pageHeight - 30, CROSSPOINT_VERSION);
